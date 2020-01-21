80beefdab08a5a807c9b6600f30e8664ab9344ec
Sahbi Chaieb <Sahbi.Chaieb@sas.com>
2020-01-21 11:05:46 +0100

    add register pipeline example

---
+++ open_source_code_node/register_sklearn_pipeline/register_sklearn_pipeline.py
@@ -0,0 +1,42 @@
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+
+from sasctl import Session
+from sasctl.tasks import register_model
+
+
+# Create X and y for training
+X_train = dm_traindf.loc[:, dm_input]
+y_train = dm_traindf[dm_dec_target]
+
+# Preprocessing interval variables
+numeric_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='median'))])
+
+# Preprocessing categorical variables
+categorical_transformer = Pipeline(steps=[
+     ('imputer', SimpleImputer(strategy='most_frequent')),
+     ('onehot', OneHotEncoder(handle_unknown='ignore'))])
+
+# Create preprocessing step
+preprocessor = ColumnTransformer(transformers=[
+    ('num', numeric_transformer, dm_interval_input),
+    ('cat', categorical_transformer, dm_class_input)])
+
+# Define pipeline steps
+dm_model = Pipeline(steps=[
+('preprocessor', preprocessor),
+('model', GradientBoostingClassifier())])
+
+fit = dm_model.fit(X_train, y_train)
+
+# Score full data
+scored = dm_model.predict_proba(dm_inputdf)
+dm_scoreddf = pd.DataFrame(scored, columns=['P_' + dm_dec_target + level for level in dm_classtarget_level])
+
+# Register model
+with Session('http://sasserver.demo.sas.com', 'username', 'password'):
+    register_model(dm_model, 'OS Node sklearn', 'My MM Project Name')
